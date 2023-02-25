01776c467d50981e78de1d13503074ca237dd11e
quadang21cen <quadang21cen@gmail.com>
2023-02-25 19:11:28 +0700

    new

--- Sensor/Sensor.ino
+++ Sensor/Sensor.ino
@@ -1,14 +1,19 @@
 
 int sensorPin=7;
 boolean val=1;   // 0 is sound, 1 is not
-
+int led = 13;
 void setup(){
   pinMode(sensorPin,INPUT);
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
   Serial.begin(9600);
 }
  
 void loop(){
- delay(1000);
  val=digitalRead(sensorPin);
  Serial.println(val);
+ if (val == 0) digitalWrite(led, HIGH);
+ else digitalWrite(led, LOW);
+ delay(1000);
+ 
 }
--- main/main.ino
+++ main/main.ino
@@ -4,37 +4,138 @@ char msg;
 char call;
 boolean st_led = false;
 int led = 13;
+int sensorPin=7;
+boolean val=1;   // 0 is sound, 1 is not
 
-void setup()
-{
-  mySerial.begin(9600);   // Setting the baud rate of GSM Module  
-  Serial.begin(9600);    // Setting the baud rate of Serial Monitor (Arduino)
+
+void setup(){
   Serial.println("GSM SIM800A BEGIN");
+  pinMode(sensorPin,INPUT);
   pinMode(led, OUTPUT);
   digitalWrite(led, LOW);
   delay(100);
+  mySerial.begin(9600);   // Setting the baud rate of GSM Module  
+  Serial.begin(9600);    // Setting the baud rate of Serial Monitor (Arduino)
 }
 
 void loop(){
-//Serial.println(st_led);
-Serial.println(mySerial.read());
-delay(1000);
-//if (mySerial.available()>0){
-//    Serial.println("RING");
-//    if (st_led = false) st_led = true;
-//    else st_led = false;
-//    if (st_led = true) digitalWrite(led, HIGH);
-//    else digitalWrite(led, LOW);
-//    
-//    while (mySerial.read()>0){
-//    delay(1000);
-//    }
+  val=digitalRead(sensorPin);   // sound sensor
+  Serial.println(val);
+  delay(1000);
+//  
+//  Serial.println(mySerial.read());
+//  Serial.println(analogRead(A0));
+//  if (Serial.available()>0)
+//   switch(Serial.read()){
+//    case 's':
+//      SendMessage();
+//      break;
+//    case 'c':
+//      MakeCall();
+//      break;
+//    case 'h':
+//      HangupCall();
+//      break;
+//    case 'e':
+//      RedialCall();
+//      break;
+//    case 'i':
+//      ReceiveCall();
+//      break;
+//    case 'r':
+//      ReceiveMessage();
+//      break;
 //  }
+//  
+// if (mySerial.available()>0){
+//  if (st_led == false){
+//    st_led = true;
+//    digitalWrite(led, HIGH);
+//  }else {
+//    st_led = false;
+//    digitalWrite(led, LOW);
+//  }
+//    Serial.write(mySerial.read());
+//  do{
+//  Serial.println(mySerial.read());
+//  delay(1000);
+// }while(mySerial.read()>0);
+//  }
+//  delay(100);
 }
-void check_sign(){
-  if (st_led = false) st_led = true;
-  else st_led = false;
-  while (mySerial.read()>0){
-    delay(1000);
+
+
+void SendMessage()
+{
+  mySerial.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+  delay(1000);  // Delay of 1000 milli seconds or 1 second
+  mySerial.println("AT+CMGS=\"+YYxxxxxxxxxx\"\r"); // Replace x with mobile number
+  delay(1000);
+  mySerial.println("sim800a sms");// The SMS text you want to send
+  delay(100);
+   mySerial.println((char)26);// ASCII code of CTRL+Z
+  delay(1000);
+}
+
+void ReceiveMessage()
+{
+  mySerial.println("AT+CNMI=2,2,0,0,0"); // AT Command to recieve a live SMS
+  delay(1000);
+  if (mySerial.available()>0)
+  {
+    msg=mySerial.read();
+    Serial.print(msg);
   }
+}
+
+void MakeCall()
+{
+  mySerial.println("ATD+84946343998;"); // ATDxxxxxxxxxx; -- watch out here for semicolon at the end!!
+  Serial.println("Calling  "); // print response over serial port
+  delay(1000);
+}
+
+
+void HangupCall()
+{
+  mySerial.println("ATH");
+  Serial.println("Hangup Call");
+  delay(1000);
+}
+
+void ReceiveCall()
+{
+  mySerial.println("ATA");
+  delay(1000);
+  {
+    call=mySerial.read();
+    Serial.print(call);
+  }
+}
+
+void RedialCall()
+{
+  mySerial.println("ATDL");
+  Serial.println("Redialing");
+  delay(1000);
+}
+
+
+void checkStatus(){
+  char st_A[100] = "STATUS A is OK"  ;
+  char st_B[100] = "STATUS B is ERR" ;
+  SendMessage();
+
+}
+
+void ctr_led(){
+if (st_led == true) {
+  digitalWrite(led, HIGH);
+  st_led = false;
+}
+else {
+  digitalWrite(led, LOW);
+  st_led = true;
+}
+
 }
