2c79d60a0a2c1da97ba8beb7ee91f100253a622c
quadang21cen <quadang21cen@gmail.com>
2023-02-25 22:00:42 +0700

    Done Sensor

--- main/main.ino
+++ main/main.ino
@@ -4,9 +4,10 @@ char msg;
 char call;
 boolean st_led = false;
 int led = 13;
-int sensorPin=7;
+int sensorPin=12;
+int ledST = 0;
 boolean val=1;   // 0 is sound, 1 is not
-
+boolean Fan_Signal = 1; 
 
 void setup(){
   Serial.println("GSM SIM800A BEGIN");
@@ -22,6 +23,64 @@ void loop(){
   val=digitalRead(sensorPin);   // sound sensor
   Serial.println(val);
   delay(1000);
+  Serial.println(val);
+  if (val == 1){
+    Serial.println(val);
+    int i = 0;
+    int num_of_DIFF = 0;
+    do{
+      val=digitalRead(sensorPin);
+      Serial.println(val);
+      if (val == 1) num_of_DIFF ++;
+      delay(500);
+      i++;
+    }while(i<=20);
+
+    int check = 20 - num_of_DIFF;
+    if(check > 2){
+      Serial.println("Hoat Dong");
+//      
+      Serial.println(num_of_DIFF);
+    }else{
+      Serial.println("khong hoat dong");
+      MakeCall();
+    }
+  }
+  
+  
+
+  
+//  if (val != new_sensor){
+//    int i = 0;
+//    int num_of_DIFF = 0;
+//    do{
+//        new_sensor = digitalRead(sensorPin);
+//        Serial.println(new_sensor);
+//        delay(1000);
+//        if (new_sensor != val){
+//           num_of_DIFF ++;
+//        }
+//        i++;
+//    }while(i<=10);
+//    Serial.println("TURN OFF");
+//    if (num_of_DIFF >= 5){
+//        if(ledST == 0){
+//          digitalWrite(led, HIGH);
+//          int ledST = 1;
+//        }
+//        
+//        else {
+//          digitalWrite(led, LOW);
+//          int ledST = 0;
+//        }
+//    }
+  }
+  
+  
+
+
+
+  
 //  
 //  Serial.println(mySerial.read());
 //  Serial.println(analogRead(A0));
@@ -62,7 +121,6 @@ void loop(){
 // }while(mySerial.read()>0);
 //  }
 //  delay(100);
-}
 
 
 void SendMessage()
@@ -90,7 +148,7 @@ void ReceiveMessage()
 
 void MakeCall()
 {
-  mySerial.println("ATD+84946343998;"); // ATDxxxxxxxxxx; -- watch out here for semicolon at the end!!
+  mySerial.println("ATD+84946345543;"); // ATDxxxxxxxxxx; -- watch out here for semicolon at the end!!
   Serial.println("Calling  "); // print response over serial port
   delay(1000);
 }
